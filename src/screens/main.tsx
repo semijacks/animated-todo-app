6c0a5f226d5e37d9e07586ceb6e69a47365f1861
Semilore Idowu <semijacks27@gmail.com>
2022-01-20 10:15:56 +0100

    added dark mode support with nativebase

---
+++ src/screens/main.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import {
+  Text,
+  Box,
+  Center,
+  VStack,
+  themeTools,
+  useTheme,
+  useColorMode,
+  useColorModeValue
+} from 'native-base'
+import ThemeToggle from '../components/theme-toggle'
+
+export default function MainScreen() {
+  return (
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box>
+          <Text>Hello</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  )
+}
